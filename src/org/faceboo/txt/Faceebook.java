2c73f0e8cab483f6045c08d1bc91aed0d3838e8d
velladuraip <velladurai00514@gmail.com>
2022-02-01 16:59:06 +0530

    commit Message

---
+++ src/org/faceboo/txt/Faceebook.java
@@ -0,0 +1,10 @@
+package org.faceboo.txt;
+
+public class Faceebook {
+	public static void main(String[] args) {
+		System.out.println("This is java");
+		System.out.println("This is dotnet");
+		System.out.println("This is Csharp");
+	}
+
+}
